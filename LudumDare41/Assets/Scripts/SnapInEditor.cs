b00810f763f34dc61b3f6338491b3db41abf6832
Jim Niehues <jniehues@5thplanetgames.com>
2018-04-21 17:18:16 -0700

    Snap in editor

---
+++ LudumDare41/Assets/Scripts/SnapInEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapInEditor : MonoBehaviour
+{
+	private void Update()
+	{
+#if UNITY_EDITOR
+		if (Application.isPlaying)
+			return;
+
+		int modX = Mathf.RoundToInt(transform.position.x / 32);
+		int modY = Mathf.RoundToInt(transform.position.y / 32);
+		transform.position = new Vector3(modX * 32, modY * 32);
+#endif
+	}
+}
