bb8f087041378244f70b10705e761f998e10b0b7
Jim Niehues <jniehues@5thplanetgames.com>
2018-04-22 12:00:32 -0700

    Fade to black on lvl change

---
+++ LudumDare41/Assets/Scripts/FullScreenFade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FullScreenFade : MonoBehaviour
+{
+	public Image image;
+	
+}
