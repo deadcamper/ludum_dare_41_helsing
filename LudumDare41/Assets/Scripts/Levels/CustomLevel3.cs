741e983b7f19ab08b5d0f0fd8004f9b7d6d19e48
Norris <norris.eldridge@gmail.com>
2018-04-22 19:52:43 -0700

    work on level 3

---
+++ LudumDare41/Assets/Scripts/Levels/CustomLevel3.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CustomLevel3 : MonoBehaviour
+{
+    public GameObject[] AmbushSpawnPoints;
+    public Enemy enemySpawnTemplate;
+
+    public void SpawnAmbush()
+    {
+        TurnManager turns = FindObjectOfType<TurnManager>();
+
+        foreach (GameObject spawn in AmbushSpawnPoints)
+        {
+            Enemy enemy = Instantiate(enemySpawnTemplate, spawn.transform.position, Quaternion.identity);
+            TurnTaker turnTaker = enemy.GetComponent<TurnTaker>();
+
+            if (turnTaker != null)
+            {
+                turns.AddTurnTaker(enemy.GetComponent<TurnTaker>());
+            }
+        }
+    }
+}
