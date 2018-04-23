1a4109f895cd7af324c737621f70333af6cc5d23
dnewel <dnewel@5thplanetgames.com>
2018-04-22 21:35:18 -0700

    Epic Finale!
    Also, Epic Optimization! WISH ME LUCK!

---
+++ LudumDare41/Assets/Scripts/EpicFinale.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+public class EpicFinale : MonoBehaviour
+{
+	void Start()
+	{
+        PrepForFinale();
+    }
+
+
+    public void PrepForFinale()
+    {
+        Songs snd = FindObjectOfType<Songs>();
+
+        if(snd != null)
+            snd.SetSong(null);
+    }
+
+    public void TriggerFinale()
+    {
+        Enemy.BYPASS_TURN_DURATION = true; //SPEED UP
+
+        Songs snd = FindObjectOfType<Songs>();
+
+        if (snd != null)
+            snd.SetSong(snd.finaleSong);
+    }
+}
